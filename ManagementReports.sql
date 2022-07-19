5feb289c219c3b69ec54dd934f4fd433f6cbb380
Carlos Wagner <61124810+carloswagner1@users.noreply.github.com>
2022-07-19 08:17:00 -0300

    update 19-07

--- ManagementReports.sql
+++ ManagementReports.sql
@@ -1,8 +1,7 @@
 USE ControleFinanceiro
 
 -- SELECIONAR ID, NOME E EMAIL DOS CLIENTES FREE QUE MIGRARAM PARA O PLANO PREMIUM ATÉ A DATA DE HOJE E ESTÃO ATIVOS
-CREATE 
-OR ALTER VIEW [dbo].[FreeToPremium] AS
+
 SELECT
     [dbo].[Cliente].ClienteId,
     [dbo].[Cliente].NomeCompleto,
@@ -21,17 +20,14 @@ INTERSECT
         [dbo].[Cliente].Email
     FROM
         [dbo].[Cliente]
-        inner join [dbo].[Assinatura] ON [dbo].[Cliente].ClienteId = [dbo].[Assinatura].ClienteId
+        LEFT JOIN [dbo].[Assinatura] ON [dbo].[Cliente].ClienteId = [dbo].[Assinatura].ClienteId
     WHERE
         [dbo].[Assinatura].PlanoId = 2
 ) 
-GO
 
-SELECT * FROM [dbo].[FreeToPremium]
 
 -- SELECIONAR ID, NOME E EMAIL DOS CLIENTES PREMIUM QUE RENOVARAM A ASSINATURA ATÉ A DATA DE HOJE E ESTÃO ATIVOS
-CREATE 
-OR ALTER VIEW [dbo].[RenewedPremiumCustomers] AS
+
 SELECT
     [dbo].[Cliente].ClienteId,
     [dbo].[Cliente].NomeCompleto,
@@ -55,13 +51,10 @@ INTERSECT
         [dbo].[Assinatura].PlanoId = 2
         AND [dbo].[Assinatura].Ativo = 1
 )
-GO
 
-SELECT * FROM [dbo].[RenewedPremiumCustomers]
 
--- SELECIONAR ID, NOME E EMAIL DOS CLIENTES PREMIUM QUE NÃO RENOVARAM A ASSINATURA ATÉ A DATA DE HOJE E ESTÃO ATIVOS
-CREATE
-OR ALTER VIEW [dbo].[NonRenewedPremiumCustomers] AS
+-- SELECIONAR ID, NOME E EMAIL DOS CLIENTES PREMIUM QUE NÃO RENOVARAM A ASSINATURA ATÉ A DATA DE HOJE 
+
 SELECT
     [dbo].[Cliente].ClienteId,
     [dbo].[Cliente].NomeCompleto,
@@ -84,11 +77,8 @@ WHERE
     )
 GO
 
-SELECT * FROM [dbo].[NonRenewedPremiumCustomers]
-
 --SELECIONAR NOME E EMAIL DOS CLIENTES QUE AS ASSINATURAS QUE VENCEM NOS PRÓXIMOS 30 DIAS
-CREATE
-OR ALTER VIEW [dbo].[SubscriptionsExpiring30Days] AS
+
 SELECT
     [dbo].[Cliente].NomeCompleto,
     [dbo].[Cliente].Email,
@@ -104,40 +94,21 @@ WHERE
     AND [dbo].[Assinatura].Ativo = 1
 GO
 
-SELECT * FROM [dbo].[SubscriptionsExpiring30Days]
-
--- CONTABILIZAR A QUANTIDADE DE PLANOS ATIVOS PASSANDO O NOME DO PLANO COMO VARIÁVEL
-CREATE
-OR ALTER PROCEDURE ActiveCustomersPerPlan @PLANO NVARCHAR(30) AS
-SELECT
-    COUNT(*) AS 'TOTAL DE PLANOS ATIVOS'
-FROM
-    [dbo].[Assinatura]
-    INNER JOIN [dbo].[PlanoDeServicos] ON [dbo].[Assinatura].PlanoId = [dbo].[PlanoDeServicos].PlanoId
-WHERE
-    UPPER([dbo].[PlanoDeServicos].[NomePlano]) = UPPER(@PLANO)
-    AND [dbo].[Assinatura].ativo = 1
-GO
-
-EXEC ActiveCustomersPerPlan @PLANO = 'Pessoa Física Premium'
-
-
 -- DEMONSTRAR TOTAIS DE PLANOS ATIVOS E QUANTIDADE DE CADA PLANO EM UMA TABELA
-CREATE OR ALTER PROCEDURE ActiveCustomersTable @Ativo BIT 
-AS
+
 DECLARE @PessoaFisicaPremium INT
 DECLARE @PessoaFisicaFree INT
 DECLARE @PessoaJuridicaPremium INT
 DECLARE @Total INT
  
 SELECT @PessoaFisicaFree = COUNT(*) FROM [dbo].[Assinatura]
-WHERE [dbo].[Assinatura].PlanoId = 1  AND [dbo].[Assinatura].Ativo = @Ativo
+WHERE [dbo].[Assinatura].PlanoId = 1  AND [dbo].[Assinatura].Ativo = 1
 
 SELECT @PessoaFisicaPremium = COUNT(*) FROM [dbo].[Assinatura]
-WHERE [dbo].[Assinatura].PlanoId = 2  AND [dbo].[Assinatura].Ativo = @Ativo
+WHERE [dbo].[Assinatura].PlanoId = 2  AND [dbo].[Assinatura].Ativo = 1
 
 SELECT @PessoaJuridicaPremium = COUNT(*) FROM [dbo].[Assinatura]
-WHERE [dbo].[Assinatura].PlanoId = 3  AND [dbo].[Assinatura].Ativo = @Ativo
+WHERE [dbo].[Assinatura].PlanoId = 3  AND [dbo].[Assinatura].Ativo = 1
  
 SET @Total = @PessoaFisicaFree + @PessoaFisicaPremium + @PessoaJuridicaPremium
 
@@ -147,7 +118,63 @@ SELECT @Total AS "Total de planos",
        @PessoaJuridicaPremium AS "Qtd pessoa juridic premium"
 GO
 
-EXEC ActiveCustomersTable @Ative = 1
+
+-- FATURAMENTO DOS ÚLTIMOS 30 DIAS
+DECLARE @PessoaFisicaPremium INT 
+DECLARE @PessoaJuridicaPremium INT 
+DECLARE @TotalPessoaFisicaPremium FLOAT 
+DECLARE @TotalPessoaJuridicaPremium FLOAT 
+DECLARE @ValorPlanoPF FLOAT 
+DECLARE @ValorPlanoPJ FLOAT 
+DECLARE @FaturamentoTotal FLOAT
+    
+SELECT
+    @PessoaFisicaPremium = COUNT(*)
+FROM
+    [dbo].[Assinatura]
+WHERE
+    [dbo].[Assinatura].PlanoId = 2
+    AND DataHoraInclusao BETWEEN DATEADD(Day, -30, GETDATE()) 
+	AND GETDATE();
+
+SELECT
+    @PessoaJuridicaPremium = COUNT(*)
+FROM
+    [dbo].[Assinatura]
+WHERE
+    [dbo].[Assinatura].PlanoId = 3
+    AND DataHoraInclusao BETWEEN DATEADD(Day, -30, GETDATE()) 
+	AND GETDATE();
+
+SELECT
+    @ValorPlanoPF = [dbo].[PlanoDeServicos].Valor
+FROM
+    [dbo].[PlanoDeServicos]
+WHERE
+    [dbo].[PlanoDeServicos].PlanoId = 2;
+
+SELECT
+    @ValorPlanoPJ = [dbo].[PlanoDeServicos].Valor
+FROM
+    [dbo].[PlanoDeServicos]
+WHERE
+    [dbo].[PlanoDeServicos].PlanoId = 3;
+
+SET
+    @TotalPessoaFisicaPremium = @PessoaFisicaPremium * @ValorPlanoPF;
+
+SET
+    @TotalPessoaJuridicaPremium = @PessoaJuridicaPremium * @ValorPlanoPJ;
+
+SET
+    @FaturamentoTotal = @TotalPessoaFisicaPremium + @TotalPessoaJuridicaPremium;
+
+SELECT
+    @FaturamentoTotal AS "Faturamento Total do Período",
+    @TotalPessoaFisicaPremium AS "Valor Total de Pessoa Física Premium",
+    @TotalPessoaJuridicaPremium AS "Valor Total de Pessoa Jurídica Premium"
+GO
+
 
 -- FATURAMENTO DO PERÍODO
 CREATE
@@ -212,3 +239,17 @@ EXEC FaturamentoPeriodo @DATAINICIAL = '2022-06-01', @DATAFINAL = '2022-06-30'
 
 
 
+-- CONTABILIZAR A QUANTIDADE DE PLANOS ATIVOS PASSANDO O NOME DO PLANO COMO VARIÁVEL
+CREATE
+OR ALTER PROCEDURE ActiveCustomersPerPlan @PLANO NVARCHAR(30) AS
+SELECT
+    COUNT(*) AS 'TOTAL DE PLANOS ATIVOS'
+FROM
+    [dbo].[Assinatura]
+    INNER JOIN [dbo].[PlanoDeServicos] ON [dbo].[Assinatura].PlanoId = [dbo].[PlanoDeServicos].PlanoId
+WHERE
+    UPPER([dbo].[PlanoDeServicos].[NomePlano]) = UPPER(@PLANO)
+    AND [dbo].[Assinatura].ativo = 1
+GO
+
+EXEC ActiveCustomersPerPlan @PLANO = 'Pessoa Física Premium'
